3d7d0c51d5351569a4569e29f2987b86690ad5e7
Mark Scala <markscala@gmail.com>
2014-11-01 16:25:07 -0500

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  author = Fabricate(:author)
+  num_of_books = rand(3) + 1
+  num_of_books.times do
+    author.books << Fabricate(:book)
+  end
+end
+
+Book.first.authors << Author.last
