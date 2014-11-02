1a8d48629dcad5575b78aa3c0e9e54378a899520
Mark Scala <markscala@gmail.com>
2014-11-02 11:44:05 -0600

    refactored author name helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def fix_author_names(names_list)
+    names = names_list.map { |name| name.split.map(&:capitalize).join(" ") }
     if names_list.length > 1
-      names = names_list.map { |name| name.split.map(&:capitalize).join(" ") }
       names[0] = last_name_first(names[0])
       names.join("; ")
     else
-      last_name_first(names_list.map { |name| name.split.map(&:capitalize).join(" ") }.first)
+      last_name_first(names.first)
     end
   end
 
