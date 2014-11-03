68946b16dea44f1135104bb7e559b04255939524
Mark Scala <markscala@gmail.com>
2014-11-02 19:23:58 -0600

    set relation of books to texts

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ActiveRecord::Base
   validates_presence_of :title, :year
   validates_numericality_of :year, only_integer: true, greater_than: 999, less_than: 2016
 
+  has_many :texts, as: :authorable
 end
