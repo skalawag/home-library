3429c2f030b9323a34ff5ca3aa28de0d27a7b6bb
Mark Scala <markscala@gmail.com>
2014-11-03 22:11:55 -0600

    method to retrive authors of a book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ActiveRecord::Base
   validates_numericality_of :year, only_integer: true, greater_than: 999, less_than: 2016
 
   has_many :texts, as: :authorable
+
+  def authors
+    self.texts.map { |text| Author.find(text[:author_id]) }
+  end
 end
