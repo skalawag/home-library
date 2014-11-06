a55ec318653dd61e16c65b0e668e09d5b98ea3e6
Mark Scala <markscala@gmail.com>
2014-11-05 20:41:15 -0600

    find authors of articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,8 @@
 class Article < ActiveRecord::Base
   validates_presence_of :title
   has_many :texts, as: :authorable
+
+  def authors
+    self.texts.map { |text| Author.find(text[:author_id]) }
+  end
 end
