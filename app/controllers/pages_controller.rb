d1328a3a1963616f1ce8ce187a5bcef9635121a5
Mark Scala <markscala@gmail.com>
2014-11-08 07:44:19 -0600

    latest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def index
   end
+
+  def mylibrary
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#index'
+  get 'mylibrary', to: 'pages#mylibrary'
 
   resources :books, only: [:index, :show, :new, :create]
-
   resources :authors, only: [:index, :show]
+  resources :articles, only: [:index]
 end
